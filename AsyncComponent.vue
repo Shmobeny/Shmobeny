6bc4110ca89ceff8bdee9d8434c1f36070725941
Bohdan Danileichenko <mr.dogystyle@gmail.com>
2022-11-18 15:47:08 +0200

    Add files via upload

---
+++ AsyncComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const smile = ref('^_^');
+</script>
+
+<template>
+  <div class="async-complete"> {{ smile }} </div>
+</template>
+
+<style>
+  .async-complete {
+    position: absolute;
+    top: 0%;
+    left: 0%;
+    padding: 20px;
+    color: black;
+    background-color: green;
+    border-radius: 50%;
+  }
+</style>
